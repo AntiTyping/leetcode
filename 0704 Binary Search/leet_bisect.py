d55be9456e4d6ac11956b74123164b0da0bc1ea8
Andy Pliszka <andy@isp44.com>
2026-06-10 08:34:26 -0400

    704. Binary Search - adds bisect

---
+++ 0704 Binary Search/leet_bisect.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = bisect.bisect_left(nums, target)
+        if i < len(nums) and nums[i] == target:
+            return i
+        return -1
