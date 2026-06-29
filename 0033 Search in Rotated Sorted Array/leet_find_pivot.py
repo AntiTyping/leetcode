527906628f1a9559fd2fff43705ef41784f32920
Andy Pliszka <andy@isp44.com>
2026-06-29 08:47:53 -0400

    33. Search in Rotated Sorted Array:

---
+++ 0033 Search in Rotated Sorted Array/leet_find_pivot.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+
+        pivot = l
+
+        if nums[pivot] <= target and target <= nums[len(nums) - 1]:
+            l, r = pivot, len(nums) - 1
+        else:
+            l, r = 0, pivot
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return -1
