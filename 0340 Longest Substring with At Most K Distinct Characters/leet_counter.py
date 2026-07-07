cf26df5fd55f325a7ea880c6dc5f46335cedf218
Andy Pliszka <andy@isp44.com>
2026-07-07 08:01:59 -0400

    340. Longest Substring with At Most K Distinct Characters

---
+++ 0340 Longest Substring with At Most K Distinct Characters/leet_counter.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        c = Counter()
+        l = ans = 0
+        for r in range(len(s)):
+            c[s[r]] += 1
+            while len(c) > k:
+                c[s[l]] -= 1
+                if c[s[l]] == 0:
+                    del c[s[l]]
+                l += 1
+            ans = max(ans, r - l + 1)
+        return ans
