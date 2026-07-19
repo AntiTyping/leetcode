bf4eb23a3a7fe2383877e21e2b1035a06e527e6a
Andy Pliszka <andy@isp44.com>
2026-07-19 10:35:44 -0400

    938. Range Sum of BST

---
+++ 0938 Range Sum of BST/leet_faster_dfs.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            ans = 0
+            if low <= node.val and node.val <= high:
+                ans += node.val
+
+            if low < node.val:
+                ans += dfs(node.left)
+            if node.val < high:
+                ans += dfs(node.right)
+
+            return ans
+
+        return dfs(root)
---
+++ 0938 Range Sum of BST/leet_iterative_faster.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        ans = 0
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if low <= node.val and node.val <= high:
+                ans += node.val
+            if low < node.val and node.left:
+                stack.append(node.left)
+            if node.val < high and node.right:
+                stack.append(node.right)
+
+        return ans
