e37976ec349028142d5b37384946bb7179669e71
Andy Pliszka <andy@isp44.com>
2026-07-20 21:43:26 -0400

    2368. Reachable Nodes With Restrictions

---
+++ 2368 Reachable Nodes With Restrictions/leet.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def reachableNodes(self, n, edges, restricted):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type restricted: List[int]
+        :rtype: int
+        """
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        rest = set(restricted)
+
+        self.ans = 0
+        self.seen = set()
+
+        def dfs(node):
+            self.seen.add(node)
+            if node in rest:
+                return
+            self.ans += 1
+            for n in graph[node]:
+                if n not in self.seen:
+                    dfs(n)
+
+        dfs(0)
+
+        return self.ans
